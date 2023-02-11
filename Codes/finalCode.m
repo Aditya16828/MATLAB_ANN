68d1e7e98040979568c4f58abc881b71f92a2fc5
Aditya Mukherjee <adimukh2001@gmail.com>
2023-02-11 08:42:33 +0530

    Added Assignment final Codes

--- Codes/finalCode.m
+++ Codes/finalCode.m
@@ -1,23 +1,24 @@
-[x, target] = bodyfat_dataset();
+%[x, target] = bodyfat_dataset();
+[x, target] = bioReactor();
 
-resMaxP = 0;
+resMaxP = 1e7;
 maxSize = 1;
 
-while (maxSize <= 60)
+while (maxSize <= 50)
     [PTrainarr, PValarr, PTestarr, net, tr] = determineHiddenLayerSize(x, target, maxSize);
 
 
-    maxValue = 0;
+    maxValue = 1e7;
     maxValueidx = -1;
     
     for i = 1:maxSize
-        if(maxValue < PTestarr(i))
+        if(maxValue > PTestarr(i))
             maxValue = PTestarr(i);
             maxValueidx = i;
         end
     end
 
-    if (resMaxP < maxValue)
+    if (resMaxP > maxValue)
         resMaxP = maxValue;
         network = net;
     end
@@ -28,11 +29,9 @@ end
 disp(resMaxP);
 
 y = network(x);
-e = gsubtract(target, y);
 
 figure, plotperform(tr);
 figure, plottrainstate(tr);
-figure, ploterrhist(e);
 figure, plotregression(target,y);
 
 genFunction(network,'myNeuralNetworkFunction');
